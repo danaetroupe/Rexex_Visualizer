2332e08c19350e416add2cf62313cb3982030d34
Danae Troupe <danaetroupe@gmail.com>
2024-04-18 14:30:40 -0400

    Match working for console

--- regex.js
+++ regex.js
@@ -1,5 +1,25 @@
 // Global Variables
 
-let input = "Regex is fun!"
-let pattern = "[a-zA-Z ]+!"
 
+function match()
+{
+    let input = document.getElementById("input-text").value;
+    let pattern = new RegExp(document.getElementById("regex-pattern").value);
+
+
+    pattern.exec(input) == input
+        ? console.log("Pattern matches.") :
+        console.log("Pattern does not match.");
+}
+
+function search()
+{
+    let input = document.getElementById("input-text").value;
+    let pattern = new RegExp(document.getElementById("regex-pattern").value, 'g');
+
+    let array1;
+
+    while ((array1 = pattern.exec(input)) !== null) {
+        console.log(`Found ${array1[0]}. Next starts at ${pattern.lastIndex}.`);
+    }
+}
