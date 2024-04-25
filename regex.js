0e0c1934dac50e5b1508dfaa5b35ffadb00e542b
mia-williams <mia.williams@spelman.edu>
2024-04-25 14:02:55 -0400

    changes

--- regex.js
+++ regex.js
@@ -5,11 +5,18 @@ function match()
 {
     let input = document.getElementById("input-text").value;
     let pattern = new RegExp(document.getElementById("regex-pattern").value);
+    let resultElement = document.getElementById("regex-canvas");
 
+    let matches = input.match(pattern);
 
-    pattern.exec(input) == input
-        ? console.log("Pattern matches.") :
-        console.log("Pattern does not match.");
+    if (matches !== null) {
+        resultElement.innerHTML = "<strong>Pattern matches:</strong><br>";
+        matches.forEach(match => {
+            resultElement.innerHTML += `<p>"${match}" matches "${pattern}"</p>`;
+        });
+    } else {
+        resultElement.textContent = "Pattern does not match.";
+    }
 }
 
 function search()
